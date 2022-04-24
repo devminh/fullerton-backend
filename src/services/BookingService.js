10e10d2b87f9a5cdeee7ec10aa68204a191b30fa
minh.vu <minh.vu@be.com.vn>
2022-04-25 00:43:28 +0700

    feat: implement queries for pagination

--- src/services/BookingService.js
+++ src/services/BookingService.js
@@ -10,10 +10,35 @@ class BookingService {
     if (data.event_location) {
       data["event_location"] = new RegExp("^" + data.event_location, "i");
     }
+    let offset = 0;
+    let limit = 10;
+
+    if (data.offset >= 0 && data.limit) {
+      offset = data.offset;
+      limit = data.limit;
+      delete data.offset;
+      delete data.limit;
+    }
 
     try {
-      const result = await this.model.find(data || {});
-      return result;
+      const result = await this.model
+        .find(data || {})
+        .sort({ updatedAt: -1 })
+        .skip(offset)
+        .limit(limit);
+
+      let total;
+
+      if (data.user_id) {
+        total = await this.model.countDocuments({ user_id: data.user_id });
+      } else {
+        total = await this.model.countDocuments();
+      }
+
+      return {
+        total: total,
+        bookings: result,
+      };
     } catch (errors) {
       throw errors;
     }
